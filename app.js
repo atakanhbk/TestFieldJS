3e6800abbc7f472deac37f743c839b75417772dc
atakanhbk <atakanhbk@gmail.com>
2023-10-27 16:35:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -173,7 +173,7 @@
 
 //   for (let i = 0; i < array.length; i++) {
 //     if(array[i] < minNumber){
-//      minNumber = array[i];   
+//      minNumber = array[i];
 //     }
 
 //     if(array[i] > maxNumber){
@@ -186,5 +186,19 @@
 
 // FindMaxAndMin();
 
+//Reverse A Word
 
+// program to generate fibonacci series up to n terms
 
+// take input from the user
+const number = parseInt(prompt('Enter the number of terms: '));
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
